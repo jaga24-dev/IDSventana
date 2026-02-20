f14a206a315f271037f911d6c40e13cd155023cd
jaga24-dev <jugo_24@alu.uabcs.mx>
2026-02-19 17:26:16 -0700

    Mostrando  ventana

---
+++ src/Ventana/Main.java
@@ -0,0 +1,10 @@
+package Ventana;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Ventana miventana=new Ventana();
+	}
+
+}
--- src/Ventana/Ventana.java
+++ src/Ventana/Ventana.java
@@ -1,5 +1,22 @@
 package Ventana;
 
-public class Ventana {
+import java.awt.Color;
+import java.awt.Dimension;
 
+import javax.swing.JFrame;
+
+
+public class Ventana extends JFrame {
+	public Ventana() {
+		this.setVisible(true);
+		this.setSize(500, 500);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		//this.setLocationRelativeTo(null);
+		this.setMinimumSize(new Dimension(200,200));
+		this.setMaximumSize(new Dimension(800,800));
+		this.setTitle("hola");
+		this.setBackground(Color.BLACK);
+		this.setLocation(200,200);
+	}
 }
+
