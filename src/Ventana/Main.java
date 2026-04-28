36cce054008a893008929ec5e326200f80e14a20
jaga24-dev <jugo_24@alu.uabcs.mx>
2026-04-28 12:42:25 -0700

    implementacion mvc
    Ejercicio 4 o Login con estructura MVC

--- src/Ventana/Main.java
+++ src/Ventana/Main.java
@@ -1,12 +1,16 @@
 package Ventana;
 
+import Views.AuthViews;
+
+
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		Ventana miventana=new Ventana();
+		//Ventana miventana=new Ventana();
 		//miventana.calculadora();
 		//miventana.registro();
 		//miventana.users();
+		AuthViews auth = new AuthViews(); 
 	}
 }
